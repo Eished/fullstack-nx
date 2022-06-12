f6c3cc8737af042bc83123ba8bdf4406e0d27d4b
eished <kished@outlook.com>
2022-06-12 22:07:45 +0800

    add express-api next-demo

--- apps/react-test/src/app/app.tsx
+++ apps/react-test/src/app/app.tsx
@@ -1,10 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Header } from '@fullstack/header';
 import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <>
+      <Header />
       <NxWelcome title="react-test" />
       <div />
     </>
---
+++ libs/header/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'header',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/header',
+};
